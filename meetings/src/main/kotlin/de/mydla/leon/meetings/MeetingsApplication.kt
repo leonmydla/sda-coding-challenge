269adf4e8a56412ee707149675627c1a67db1185
Leon Mydla <leon@mydla.dev>
2022-04-10 21:43:17 +0200

    add spring boot backend

---
+++ meetings/src/main/kotlin/de/mydla/leon/meetings/MeetingsApplication.kt
@@ -0,0 +1,11 @@
+package de.mydla.leon.meetings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetingsApplication
+
+fun main(args: Array<String>) {
+    runApplication<MeetingsApplication>(*args)
+}
---
+++ meetings/src/test/kotlin/de/mydla/leon/meetings/MeetingsApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.mydla.leon.meetings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeetingsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
