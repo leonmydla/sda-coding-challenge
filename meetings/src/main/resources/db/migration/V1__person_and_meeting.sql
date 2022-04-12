2c2391c457296b4f79530f048fb1da52825200fb
Leon Mydla <leon@mydla.dev>
2022-04-12 03:57:50 +0200

    update table definitions

--- meetings/src/main/resources/db/migration/V1__person_and_meeting.sql
+++ meetings/src/main/resources/db/migration/V1__person_and_meeting.sql
@@ -3,14 +3,14 @@ CREATE TABLE IF NOT EXISTS person (
     name VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (id)
-    );
+);
 
 CREATE TABLE IF NOT EXISTS meeting (
     id          BIGSERIAL,
     person_id   BIGINT      NOT NULL,
-    date_time   TIMESTAMP   NOT NULL,
+    date_time   TIMESTAMP NOT NULL,
     coordinates VARCHAR(64) NOT NULL,
 
     PRIMARY KEY (id),
     CONSTRAINT fk_meeting_person_id FOREIGN KEY (person_id) REFERENCES person (id) ON UPDATE RESTRICT ON DELETE RESTRICT
-    );
+);
