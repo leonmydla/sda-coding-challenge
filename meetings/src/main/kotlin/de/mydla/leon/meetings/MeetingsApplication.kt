fad009525b687d3baa0368a462968a827ead4cf3
Leon Mydla <leon@mydla.dev>
2022-04-12 06:14:03 +0200

    fix application main fn

--- meetings/src/main/kotlin/de/mydla/leon/meetings/MeetingsApplication.kt
+++ meetings/src/main/kotlin/de/mydla/leon/meetings/MeetingsApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MeetingsApplication
+open class MeetingsApplication
 
 fun main(args: Array<String>) {
     runApplication<MeetingsApplication>(*args)
