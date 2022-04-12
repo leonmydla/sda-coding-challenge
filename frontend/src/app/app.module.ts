c860df58bb6d9130dc16d39c4bdf7cd81614e3de
Leon Mydla <leon@mydla.dev>
2022-04-12 02:07:22 +0200

    utilize the add form and overview table

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddMeetingModule } from './add-meeting/add-meeting.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeetingsOverviewModule } from './meetings-overview/meetings-overview.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule(
+  {
+    declarations: [
+      AppComponent
+    ],
+    imports     : [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MeetingsOverviewModule,
+      AddMeetingModule
+    ],
+    providers   : [],
+    bootstrap   : [AppComponent]
+  })
+export class AppModule {}
